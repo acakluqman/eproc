d5ba279087c165e29156cc2fa22f493fa5d32521
luqman <luqmanul@its.ac.id>
2023-03-15 08:16:57 +0700

    update .gitignore

--- config.sample.php
+++ config.sample.php
@@ -3,9 +3,7 @@
 use Tamtamchik\SimpleFlash\Flash;
 
 ob_start();
-if (!session_id()) {
-    session_start();
-}
+if (!session_id()) session_start();
 
 require_once('./config.php');
 require_once('vendor/autoload.php');
@@ -13,12 +11,9 @@ require_once('function/general.php');
 
 $flash = new Flash();
 
-$_SESSION['is_login'] = true;
-$_SESSION['nama'] = 'Luqman Hakim';
-
-$db_host = '127.0.0.1';     // localhost
-$db_user = 'root';          // luqmanga_eproc
-$db_name = 'eproc';         // luqmanga_eproc
+$db_host = '127.0.0.1';
+$db_user = 'root';
+$db_name = 'eproc';
 $db_pass = 'hackyhack';     // rJkZ[-I+dTBK
 
 try {
